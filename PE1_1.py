7bcf19128c20c4b54d79b97a13b6caad2902f8ed
Kingsley.S <kingsleysiew691@gmail.com>
2024-09-10 18:42:33 -0400

    Add files via upload

---
+++ PE1_1.py
@@ -0,0 +1,2 @@
+# '\n' is used to insert a newline character between the two names.
+print("Yat Lun\nSiew")
---
+++ PE1_2.py
@@ -0,0 +1,10 @@
+# The '\t' is used to add a tab space for alignment, and '\n' adds a newline after the header.
+print("Item\t\tPrice\n")
+# The '\t' adds a tab space between the item names and prices for alignment.
+print("Apple\t\t$1.75")
+print("Banana\t\t$2.25")
+print("Cherry\t\t$3.50")
+# '\n' before the line provides space between the items and the separator.
+print("\n---------- -------")
+# '\t' aligns "Total" with the other items and prices.
+print("Total\t\t$7.50")
