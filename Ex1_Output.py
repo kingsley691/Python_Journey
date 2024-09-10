bb2210033cd887491c38d516c86bea14779bfdcf
Kingsley.S <kingsleysiew691@gmail.com>
2024-09-10 18:56:04 -0400

    Add files via upload

---
+++ Ex1_Output.py
@@ -0,0 +1,8 @@
+# Intro Examples
+# using print() function to display
+
+print("Hello World")
+print("Hello python World")
+print("Hello ET574")
+print("Hello Prof.B")
+print("Hello classmates")
---
+++ Ex2_Input.py
@@ -0,0 +1,12 @@
+# Intro Examples
+# using input() & print() function to display
+
+course  = input("Enter your course: ")
+print("Welcome to " + course + '!')
+print()
+name  = input("What is your name: ")
+print("Whassup " + name + '!')
+print()
+colour  = input("What is your favourite colour: ")
+print("My favourite color is " + colour + '!')
+print()
